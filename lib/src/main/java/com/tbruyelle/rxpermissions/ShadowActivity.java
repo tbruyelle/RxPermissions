6b818764a6457e590cd0cb48516da870933d7186
Boy Wang <boyw165@gmail.com>
2016-09-02 23:46:38 +0800

    Use support-library to handle the permissions for the `ShadowActivity`.

--- lib/src/main/java/com/tbruyelle/rxpermissions/ShadowActivity.java
+++ lib/src/main/java/com/tbruyelle/rxpermissions/ShadowActivity.java
@@ -1,13 +1,18 @@
 package com.tbruyelle.rxpermissions;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 
-@TargetApi(Build.VERSION_CODES.M)
-public class ShadowActivity extends Activity {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ShadowActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +28,32 @@ public class ShadowActivity extends Activity {
     }
 
     private void handleIntent(Intent intent) {
-        String[] permissions = intent.getStringArrayExtra("permissions");
-        requestPermissions(permissions, 42);
+        List<String> perms = Arrays.asList(intent.getStringArrayExtra("permissions"));
+        List<String> yetGrantedPerms = Collections.emptyList();
+
+        // Figure out what permissions are not granted.
+        for (String perm : perms) {
+            int check = ContextCompat.checkSelfPermission(this,
+                                                          perm);
+
+            if (PackageManager.PERMISSION_GRANTED != check) {
+                yetGrantedPerms.add(perm);
+            }
+        }
+
+        // Request permissions if any.
+        int permNum = yetGrantedPerms.size();
+        if (permNum > 0) {
+            ActivityCompat.requestPermissions(this,
+                                              yetGrantedPerms.toArray(new String[permNum]),
+                                              42);
+        }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
         RxPermissions.getInstance(this).onRequestPermissionsResult(requestCode, permissions, grantResults);
         finish();
     }
