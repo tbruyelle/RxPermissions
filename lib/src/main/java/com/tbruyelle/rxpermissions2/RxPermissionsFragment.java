2f36f765160480139b2178cda539c880dc6c76a9
Caijinglong <cjl_spy@163.com>
2019-11-27 10:48:28 +0800

    Migrate java code to androidX.

--- lib/src/main/java/com/tbruyelle/rxpermissions2/RxPermissionsFragment.java
+++ lib/src/main/java/com/tbruyelle/rxpermissions2/RxPermissionsFragment.java
@@ -4,14 +4,14 @@ import android.annotation.TargetApi;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import io.reactivex.subjects.PublishSubject;
 
 public class RxPermissionsFragment extends Fragment {
